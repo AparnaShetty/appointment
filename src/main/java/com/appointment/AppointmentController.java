9ce6cd4b9f2c1a59212f8a4da07c22063d481c0c
aparna shetty <aparnashetty16july@gmail.com>
2018-08-18 09:58:55 -0400

    Comments Added

--- src/main/java/com/appointment/AppointmentController.java
+++ src/main/java/com/appointment/AppointmentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//Controller Class to map requests
 @RestController
 @RequestMapping("/api")
 public class AppointmentController
